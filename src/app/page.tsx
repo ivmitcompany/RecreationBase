b070bc305710b4d25ca096e6a36b4765a13c6af8
Bochkovskyi <unundertale1@ukr.net>
2024-02-19 22:00:01 +0200

    update return statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      HI THERE!
+      <p className="first-letter:font-accent text-6xl font-light uppercase leading-[5.125rem] first-letter:mr-5 first-letter:text-[7.5rem] first-letter:leading-[9rem]">
+        Mеню
+      </p>
     </div>
   )
 }
