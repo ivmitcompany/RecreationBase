8b341610706d1745bf802d8ff8d7c25a365ec9fb
Bochkovskyi <unundertale1@ukr.net>
2024-02-21 21:21:39 +0200

    add Contacts component

---
+++ src/components/ui/Contacts.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/utils'
+import { FC, HTMLAttributes } from 'react'
+
+interface ContactsProps extends HTMLAttributes<HTMLDivElement> {
+  short?: boolean
+}
+
+const Contacts: FC<ContactsProps> = ({
+  className,
+  short = false,
+  ...props
+}) => (
+  <address
+    className={cn(
+      'flex flex-col justify-center gap-2.5 not-italic md:gap-1.5',
+      className
+    )}
+    {...props}
+  >
+    <a className="transition-colors hover:text-accent" href="tel:380678279007">
+      +380678279007
+    </a>
+    <p className={cn(short && 'sr-only')}>с.Рокосово, вул.Виноградна 14</p>
+  </address>
+)
+
+export default Contacts
