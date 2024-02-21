56d2677d5b9a4a187d3fe2dd2ebd7182c3ff8858
Bochkovskyi <unundertale1@ukr.net>
2024-02-21 18:05:21 +0200

    export interface

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { AnchorHTMLAttributes, FC } from 'react'
 import { Icons } from './Icons'
 
-interface LogoProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+export interface LogoProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
 const Logo: FC<LogoProps> = ({ className, href = '/', ...props }) => (
   <Link className={className} href={href} {...props}>
