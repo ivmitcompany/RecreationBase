34aff64b99b3d16ceeeeab52e8bad7a044fbca03
Bochkovskyi <unundertale1@ukr.net>
2024-02-21 21:56:25 +0200

    add padding extension

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
         accent: 'var(--font-accent)',
         default: 'var(--font-default)',
       },
+      padding: {
+        header: 'var(--header-height)',
+      },
     },
   },
 }
