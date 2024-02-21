f86c24e68ed746248d57a8d2ea59a211dae610fc
Bochkovskyi <unundertale1@ukr.net>
2024-02-21 21:57:33 +0200

    utilize header context for menu close

--- src/components/ui/menu/MenuItem.tsx
+++ src/components/ui/menu/MenuItem.tsx
@@ -1,6 +1,9 @@
+'use client'
+
+import HeaderContext from '@/store/header-context'
 import { cn } from '@/utils'
 import Link from 'next/link'
-import { FC, LiHTMLAttributes } from 'react'
+import { FC, LiHTMLAttributes, useContext } from 'react'
 
 interface MenuItemProps extends LiHTMLAttributes<HTMLLIElement> {
   href?: string
@@ -13,23 +16,28 @@ const MenuItem: FC<MenuItemProps> = ({
   href = '/',
   underline,
   ...props
-}) => (
-  <li
-    className={cn(
-      'uppercase transition-colors md:hover:text-accent',
-      underline && 'flex w-full items-end md:w-fit',
-      className
-    )}
-    {...props}
-  >
-    <Link href={href}>{children}</Link>
-    {underline && (
-      <span
-        aria-hidden
-        className="-mb-1.5 block h-0.5 w-full bg-light md:hidden"
-      />
-    )}
-  </li>
-)
+}) => {
+  const { closeMenu } = useContext(HeaderContext)
+
+  return (
+    <li
+      className={cn(
+        'uppercase transition-colors md:hover:text-accent',
+        underline && 'flex w-full items-end md:w-fit',
+        className
+      )}
+      onClick={closeMenu}
+      {...props}
+    >
+      <Link href={href}>{children}</Link>
+      {underline && (
+        <span
+          aria-hidden
+          className="-mb-1.5 block h-0.5 w-full bg-light md:hidden"
+        />
+      )}
+    </li>
+  )
+}
 
 export default MenuItem
