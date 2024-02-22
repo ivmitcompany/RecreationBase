8dee4987772aad6df55f3a2e7691392a9a514ded
Bochkovskyi <unundertale1@ukr.net>
2024-02-22 22:28:00 +0200

    add IconButton component

---
+++ src/components/ui/IconButton.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils'
+import { ButtonHTMLAttributes, ComponentType, FC } from 'react'
+
+import { IconProps } from './Icons'
+
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  Icon: ComponentType<IconProps>
+  label: string
+}
+
+const IconButton: FC<IconButtonProps> = ({
+  Icon,
+  children,
+  className,
+  label,
+  ...props
+}) => (
+  <button
+    className={cn(
+      'block bg-light p-1.5 text-accent transition-opacity hover:opacity-70',
+      className
+    )}
+    {...props}
+  >
+    <Icon aria-hidden="true" size="sm" />
+    <span className="sr-only">{label}</span>
+  </button>
+)
+
+export default IconButton
