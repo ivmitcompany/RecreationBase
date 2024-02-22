2d94c8a1f5efef2c6e8069a26b2f3af8014a7396
Bochkovskyi <unundertale1@ukr.net>
2024-02-22 14:05:27 +0200

    add FontAccentSpan component

---
+++ src/components/ui/FontAccentSpan.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/utils'
+import { VariantProps, cva } from 'class-variance-authority'
+import { FC, HTMLAttributes } from 'react'
+
+const fontAccentSpanVariants = cva('inline-block font-accent', {
+  defaultVariants: { size: 'default' },
+  variants: {
+    size: {
+      default: 'text-6xl leading-[1.2] md:text-[7.5rem]',
+      md: 'text-6xl leading-[1.2] md:text-[5.75rem]',
+      sm: 'text-6xl leading-[0.5] md:text-[5.625rem]',
+    },
+  },
+})
+
+interface FontAccentSpanProps
+  extends HTMLAttributes<HTMLSpanElement>,
+    VariantProps<typeof fontAccentSpanVariants> {}
+
+const FontAccentSpan: FC<FontAccentSpanProps> = ({
+  children,
+  className,
+  size,
+}) => (
+  <span className={cn(fontAccentSpanVariants({ size }), className)} role="text">
+    {children}
+  </span>
+)
+
+export default FontAccentSpan
