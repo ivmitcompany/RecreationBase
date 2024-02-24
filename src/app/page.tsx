cc29fa5752a42d580f613e3a635f00d3ec6e4a2f
Bochkovskyi <unundertale1@ukr.net>
2024-02-24 19:09:34 +0200

    utilize AboutSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from '@/components/pages/home/about/AboutSection'
 import HeroSection from '@/components/pages/home/hero/HeroSection'
 
 export default function Home() {
   return (
     <>
       <HeroSection className="pb-[8.625rem] md:pb-[10.6875rem]" />
+      <AboutSection className="pb-[6.25rem] md:pb-[7.5rem]" />
     </>
   )
 }
