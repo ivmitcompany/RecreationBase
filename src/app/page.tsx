33a3516cda4893574f591845e86f4577f6825b72
Bochkovskyi <unundertale1@ukr.net>
2024-02-26 13:50:23 +0200

    utilize ApartmentsSection component, update HeroSection styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from '@/components/pages/home/about/AboutSection'
+import ApartmentsSection from '@/components/pages/home/apartments/ApartmentsSection'
 import HeroSection from '@/components/pages/home/hero/HeroSection'
 
 export default function Home() {
   return (
     <>
-      <HeroSection className="pb-[8.625rem] md:pb-[10.6875rem]" />
+      <HeroSection className="pb-[8.625rem] pt-header md:pb-[10.6875rem]" />
       <AboutSection className="pb-[6.25rem] md:pb-[7.5rem]" />
+      <ApartmentsSection className="pb-44 pt-20 md:pb-28 md:pt-[5.6875rem]" />
     </>
   )
 }
