9fee8398dff571ec9728f9988f650d24406f0790
Bochkovskyi <unundertale1@ukr.net>
2024-02-28 19:40:07 +0200

    add button circle variants, update props usage

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,5 +1,4 @@
 import { Icons } from '@/components/ui/Icons'
-import { linkButtonVariants } from '@/components/ui/button/LinkButton'
 import { cn } from '@/utils'
 import { VariantProps, cva } from 'class-variance-authority'
 import { ButtonHTMLAttributes, FC } from 'react'
@@ -7,9 +6,9 @@ import { ButtonHTMLAttributes, FC } from 'react'
 export const buttonVariants = cva(
   'group/button flex items-center font-light uppercase',
   {
-    defaultVariants: { size: 'default' },
+    defaultVariants: { buttonSize: 'default' },
     variants: {
-      size: {
+      buttonSize: {
         default: 'text-[1.375rem] leading-[1.2] md:text-[1.625rem]',
         sm: 'text-lg',
       },
@@ -17,17 +16,53 @@ export const buttonVariants = cva(
   }
 )
 
+export const buttonCircleVariants = cva(
+  'flex items-center justify-center text-center break-words border border-accent transition-colors leading-[1.2] rounded-full font-light uppercase',
+  {
+    defaultVariants: {
+      circleColor: 'default',
+      circleSize: 'default',
+      circleSpacing: 'default',
+    },
+    variants: {
+      circleColor: {
+        default: 'text-dark md:hover:text-light md:hover:bg-accent',
+        light: 'text-light md:hover:bg-accent',
+      },
+      circleSize: {
+        default: 'h-32 w-32 md:w-44 md:h-44 px-4 md:px-8 md:text-lg',
+        md: 'w-[8.5rem] h-[8.5rem] md:w-36 md:h-36 px-[1.875rem md:text-lg',
+        sm: 'w-20 h-20 px-5',
+        xs: 'w-10 h-10 px-2.5',
+      },
+      circleSpacing: {
+        default: 'ml-5 md:ml-[1.875rem]',
+        tight: 'ml-5',
+      },
+    },
+  }
+)
+
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants>,
+    VariantProps<typeof buttonCircleVariants> {}
 
-const Button: FC<ButtonProps> = ({ children, className, size, ...props }) => (
-  <button className={cn(buttonVariants({ size }), className)} {...props}>
+const Button: FC<ButtonProps> = ({
+  buttonSize,
+  children,
+  circleColor = 'default',
+  circleSize = 'sm',
+  circleSpacing,
+  className,
+  ...props
+}) => (
+  <button className={cn(buttonVariants({ buttonSize }), className)} {...props}>
     {children}
     <span
       className={cn(
-        'ml-5 group-hover/button:bg-accent group-hover/button:text-light md:ml-[1.875rem]',
-        linkButtonVariants({ size: 'sm' })
+        'group-hover/button:bg-accent group-hover/button:text-light ',
+        buttonCircleVariants({ circleColor, circleSize, circleSpacing })
       )}
     >
       <Icons.arrow />
