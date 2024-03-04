e43d553b6478ff394eda0bc3c744d090336c687b
Bochkovskyi <unundertale1@ukr.net>
2024-03-04 21:58:46 +0200

    update return statement

--- src/components/pages/menu/info/socials/MenuInfoSocialsItem.tsx
+++ src/components/pages/menu/info/socials/MenuInfoSocialsItem.tsx
@@ -14,25 +14,23 @@ const MenuInfoSocialsItem: FC<MenuInfoSocialsItemProps> = ({
   icon: Icon,
   label,
   ...props
-}) => {
-  return (
-    <li className={cn('w-fit font-light', className)} {...props}>
-      <a className="group flex items-center gap-3" href={href}>
-        <Icon aria-hidden className="text-accent" />
-        <div className="flex flex-col gap-1">
-          <span className="text-xs" role="text">
-            {label}
-          </span>
-          <span
-            className="text-[0.875rem] leading-5 transition-colors group-hover:text-accent"
-            role="text"
-          >
-            {children}
-          </span>
-        </div>
-      </a>
-    </li>
-  )
-}
+}) => (
+  <li className={cn('w-fit font-light', className)} {...props}>
+    <a className="group flex items-center gap-3" href={href}>
+      <Icon aria-hidden className="text-accent" />
+      <div className="flex flex-col gap-1">
+        <span className="text-xs" role="text">
+          {label}
+        </span>
+        <span
+          className="text-[0.875rem] leading-5 transition-colors group-hover:text-accent"
+          role="text"
+        >
+          {children}
+        </span>
+      </div>
+    </a>
+  </li>
+)
 
 export default MenuInfoSocialsItem
