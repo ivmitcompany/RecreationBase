bc258558f46aebfada66dc57b3ea4a807080a3f4
Bochkovskyi <unundertale1@ukr.net>
2024-03-05 00:15:14 +0200

    utilize CompanyRules component

--- src/components/pages/menu/company/info/CompanyInfo.tsx
+++ src/components/pages/menu/company/info/CompanyInfo.tsx
@@ -3,13 +3,20 @@ import CompanySocials from '@/components/pages/menu/company/socials/CompanySocia
 import { cn } from '@/utils'
 import React, { FC, HTMLAttributes } from 'react'
 
+import CompanyRules from '../rules/CompanyRules'
+
 interface CompanyInfoProps extends HTMLAttributes<HTMLDivElement> {}
+
 const CompanyInfo: FC<CompanyInfoProps> = ({ className, ...props }) => {
   return (
     <div className={cn('space-y-10', className)} {...props}>
       <div className="space-y-5">
         <CompanyInfoHeading>Інформація про заклад</CompanyInfoHeading>
         <CompanySocials className="pl-[1.125rem]" />
+      </div>
+      <div className="space-y-5">
+        <CompanyInfoHeading>Правила закладу</CompanyInfoHeading>
+        <CompanyRules className="pl-[1.125rem]" />
       </div>
     </div>
   )
