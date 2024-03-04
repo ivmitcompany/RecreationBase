d31b95a84bb53641ac48eced08f1c04773686ec3
Bochkovskyi <unundertale1@ukr.net>
2024-03-04 21:54:49 +0200

    utilize isHomePage function for classname render

--- src/components/layout/header/HeaderLogo.tsx
+++ src/components/layout/header/HeaderLogo.tsx
@@ -2,14 +2,25 @@
 
 import Logo, { LogoProps } from '@/components/ui/Logo'
 import HeaderContext from '@/store/header-context'
+import { cn, isHomePage } from '@/utils'
+import { usePathname } from 'next/navigation'
 import { FC, useContext } from 'react'
 
 interface HeaderLogoProps extends LogoProps {}
 
 const HeaderLogo: FC<HeaderLogoProps> = ({ ...props }) => {
-  const { closeMenu } = useContext(HeaderContext)
+  const { closeMenu, isMenuOpened } = useContext(HeaderContext)
+  const pathname = usePathname()
 
-  return <Logo onClick={closeMenu} {...props} />
+  return (
+    <Logo
+      className={cn(
+        isMenuOpened && !isHomePage(pathname) && 'text-light transition-colors'
+      )}
+      onClick={closeMenu}
+      {...props}
+    />
+  )
 }
 
 export default HeaderLogo
