897aa319f70b21ced995efa076db500bd22d4b69
Bochkovskyi <unundertale1@ukr.net>
2024-03-05 13:23:03 +0200

    update styles

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -1,3 +1,4 @@
+import MenuHeading from '@/components/pages/menu/MenuHeading'
 import CompanyInfo from '@/components/pages/menu/company/info/CompanyInfo'
 import Content from '@/components/ui/Content'
 import { ReactNode } from 'react'
@@ -11,11 +12,16 @@ export default function MenuLayout({ children, navigation }: MenuLayoutProps) {
   return (
     <div className="pt-header">
       <Content>
-        <div className="flex gap-[3.125rem]">
-          {navigation}
-          {children}
-          <CompanyInfo />
-        </div>
+        <section className="pb-7 pt-5 md:pb-[7.1875rem] md:pt-9">
+          <MenuHeading className="mx-auto" />
+          <div className="mt-14 flex gap-10 md:mt-8">
+            <aside className="basis-[17.8125rem]">{navigation}</aside>
+            <section className="flex-shrink flex-grow basis-0">
+              {children}
+            </section>
+            <CompanyInfo className="basis-[17.8125rem]" />
+          </div>
+        </section>
       </Content>
     </div>
   )
