4f8ca595c96a75d3968419ef005a4c1adbdc5a05
Bochkovskyi <unundertale1@ukr.net>
2024-03-05 15:55:49 +0200

    add CompanyInfo component to the sidebar

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -14,10 +14,13 @@ export default function MenuLayout({ children, navigation }: MenuLayoutProps) {
       <Content>
         <section className="pb-7 pt-5 md:pb-[7.1875rem] md:pt-9">
           <MenuHeading className="mx-auto" />
-          <div className="mt-14 flex gap-10 md:mt-8">
-            <aside className="basis-[17.8125rem]">{navigation}</aside>
+          <div className="mt-14 flex flex-col gap-10 md:mt-8 md:flex-row">
+            <aside className="basis-[17.8125rem]">
+              {navigation}
+              <CompanyInfo className="lg:hidden" />
+            </aside>
             <div className="flex-shrink flex-grow basis-0">{children}</div>
-            <CompanyInfo className="basis-[17.8125rem]" />
+            <CompanyInfo className="hidden basis-[17.8125rem] lg:block" />
           </div>
         </section>
       </Content>
