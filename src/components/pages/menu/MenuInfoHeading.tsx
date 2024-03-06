95fdbef0ad8c42869dbbe20af4f0e7505d2e8df3
Bochkovskyi <unundertale1@ukr.net>
2024-03-06 17:58:19 +0200

    add level prop

--- src/components/pages/menu/MenuInfoHeading.tsx
+++ src/components/pages/menu/MenuInfoHeading.tsx
@@ -1,16 +1,26 @@
 import { cn } from '@/utils'
 import React, { FC, HTMLAttributes } from 'react'
 
-interface MenuInfoHeadingProps extends HTMLAttributes<HTMLHeadingElement> {}
+interface MenuInfoHeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  level?: 1 | 2 | 3 | 4 | 5 | 6
+}
 
 const MenuInfoHeading: FC<MenuInfoHeadingProps> = ({
   children,
   className,
+  level = 4,
   ...props
-}) => (
-  <h4 className={cn('text-lg font-light uppercase', className)} {...props}>
-    {children}
-  </h4>
-)
+}) => {
+  const HeadingTag: keyof JSX.IntrinsicElements = `h${level}`
+
+  return (
+    <HeadingTag
+      className={cn('text-lg font-light uppercase', className)}
+      {...props}
+    >
+      {children}
+    </HeadingTag>
+  )
+}
 
 export default MenuInfoHeading
