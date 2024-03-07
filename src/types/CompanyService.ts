0ff42ce413ac36ef22bc6d311cebadd6bc8274d2
Bochkovskyi <unundertale1@ukr.net>
2024-03-07 22:28:23 +0200

    update CompanyService type

--- src/types/CompanyService.ts
+++ src/types/CompanyService.ts
@@ -1,11 +1,11 @@
 import ImageInfo from './ImageInfo'
 
 type CompanyService = {
-  description?: string
-  excess_time_price: number
+  description: string
+  excess_time_price?: number
   id: number
   images?: ImageInfo[]
-  min_order_time: number
+  min_order_time?: number
   note?: string
   price: number
   title: string
