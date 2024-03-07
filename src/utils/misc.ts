1af60cc36760c3403afdaa48bc36a56b41846de9
Bochkovskyi <unundertale1@ukr.net>
2024-03-07 17:05:30 +0200

    remove zero check from zeroPadSingleDigit

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -16,9 +16,8 @@ export const isHomePage = (pathname: string) => pathname === '/'
 
 export const zeroPadSingleDigit = (num: number) => {
   const isSingleDigit = num < 10
-  const isNotZero = num !== 0
 
-  if (isSingleDigit && isNotZero) return `0${num}`
+  if (isSingleDigit) return `0${num}`
 
   return num.toString()
 }
