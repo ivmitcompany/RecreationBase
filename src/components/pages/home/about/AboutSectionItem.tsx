bef7f4afe741cd8ae9313144e6443d7242820752
Bochkovskyi <unundertale1@ukr.net>
2024-03-07 22:27:57 +0200

    utilize getMainImage utility

--- src/components/pages/home/about/AboutSectionItem.tsx
+++ src/components/pages/home/about/AboutSectionItem.tsx
@@ -11,8 +11,8 @@ import AboutItem from '@/types/AboutItem'
 import {
   arrayIsNotEmpty,
   cn,
-  findObjectWithField,
   getArrayLength,
+  getMainImage,
   zeroPadSingleDigit,
 } from '@/utils'
 import Image from 'next/image'
@@ -66,10 +66,7 @@ const AboutSectionItem: FC<AboutSectionItemProps> = ({
                 fill
                 quality={80}
                 sizes="(max-width: 767px) 100vw, (max-width: 894x) 50vw, 387px"
-                src={
-                  findObjectWithField(images!, 'is_main_image')?.image ||
-                  images![0].image
-                }
+                src={getMainImage(images!)}
               />
               {imagesCount > 1 && (
                 <IconButton
--- src/components/pages/home/services/ServicesSectionItem.tsx
+++ src/components/pages/home/services/ServicesSectionItem.tsx
@@ -6,7 +6,7 @@ import Modal from '@/components/ui/Modal'
 import Button from '@/components/ui/button/Button'
 import useModal from '@/hooks/use-modal'
 import CompanyService from '@/types/CompanyService'
-import { arrayIsNotEmpty, cn } from '@/utils'
+import { arrayIsNotEmpty, cn, getMainImage } from '@/utils'
 import Image from 'next/image'
 import { FC, HTMLAttributes } from 'react'
 
@@ -42,9 +42,8 @@ const ServicesSectionItem: FC<ServicesSectionItemProps> = ({
                 alt={title}
                 className="object-cover object-center"
                 fill
-                quality={80}
                 sizes="(max-width: 767px) 100vw, (max-width: 1279) 50vw, 488px"
-                src={images![0].image}
+                src={getMainImage(images!)}
               />
             </>
           ) : (
