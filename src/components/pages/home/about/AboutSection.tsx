3e8ad05f93be0265fbec1f991831b65d40b34b0e
Bochkovskyi <unundertale1@ukr.net>
2024-03-07 16:13:27 +0200

    add data fetching using getAboutItems utility

--- src/components/pages/home/about/AboutSection.tsx
+++ src/components/pages/home/about/AboutSection.tsx
@@ -1,40 +1,16 @@
 import Content from '@/components/ui/Content'
 import CurlyBraceHeading from '@/components/ui/CurlyBraceHeading'
-import { ImageInfo } from '@/types/ImageInfo'
 import { cn } from '@/utils'
+import { getAboutItems } from '@/utils'
 import { FC, HTMLAttributes } from 'react'
 
 import AboutSectionItem from './AboutSectionItem'
 
-export type AboutItem = {
-  id: number
-  images?: ImageInfo[]
-  title: string
-}
-
-const aboutItems: AboutItem[] = [
-  {
-    id: 9,
-    images: [
-      { direction: 'horizontal', src: '/about-img.jpg' },
-      { direction: 'vertical', src: '/hero-img.jpg' },
-    ],
-    title: 'неймовірна природа, озеленена територія',
-  },
-  {
-    id: 10,
-    title: 'святкування банкетів та виїзних церемоній',
-  },
-  {
-    id: 11,
-    images: [{ direction: 'vertical', src: '/hero-img.jpg' }],
-    title: 'святкування банкетів та виїзних церемоній',
-  },
-]
-
 interface AboutSectionProps extends HTMLAttributes<HTMLDivElement> {}
 
-const AboutSection: FC<AboutSectionProps> = ({ className, ...props }) => {
+const AboutSection: FC<AboutSectionProps> = async ({ className, ...props }) => {
+  const aboutItems = await getAboutItems()
+
   return (
     <div className={cn('bg-graphite text-light', className)} {...props}>
       <Content>
