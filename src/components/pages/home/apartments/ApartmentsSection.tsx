515e50d667e401d55a88daf6da98d28a3b5bb53d
Bochkovskyi <unundertale1@ukr.net>
2024-03-08 19:17:50 +0200

    add apartments fetch, add condition to the return statement

--- src/components/pages/home/apartments/ApartmentsSection.tsx
+++ src/components/pages/home/apartments/ApartmentsSection.tsx
@@ -1,125 +1,38 @@
 import Content from '@/components/ui/Content'
 import CurlyBraceHeading from '@/components/ui/CurlyBraceHeading'
-import Apartment from '@/types/Apartment'
+import { arrayIsNotEmpty } from '@/utils'
+import { getApartments } from '@/utils/api/apartments'
 import React, { FC, HTMLAttributes } from 'react'
 
 import ApartmentsSectionItem from './ApartmentsSectionItem'
 
-const apartments: Apartment[] = [
-  {
-    additions: [
-      'Власна ванна кміната',
-      'Туалет',
-      'Душ',
-      'Кондиціонер',
-      'Холодильник',
-      'Телевізор',
-      'Безкоштовний wi-fi',
-    ],
-    id: 1,
-    images: [
-      {
-        direction: 'horizontal',
-        image: '/apartment-1.jpg',
-        is_main_image: true,
-      },
-      {
-        direction: 'vertical',
-        image: '/apartment-2.jpg',
-        is_main_image: false,
-      },
-    ],
-    name: 'Двомісний номер',
-    price: 1600,
-  },
-  {
-    additions: [
-      'Власна ванна кміната',
-      'Туалет',
-      'Душ',
-      'Кондиціонер',
-      'Телевізор',
-      'Парковка',
-      'Безкоштовний wi-fi',
-      'Холодильник',
-    ],
-    id: 2,
-    name: 'Модульний будинок',
-    price: 6000,
-  },
-  {
-    additions: [
-      'Власна ванна кміната',
-      'Туалет',
-      'Душ',
-      'Кондиціонер',
-      'Телевізор',
-      'Безкоштовний wi-fi',
-      'Холодильник',
-      'Мангальна зона',
-    ],
-    id: 3,
-    images: [
-      {
-        direction: 'horizontal',
-        image: '/apartment-3.jpg',
-        is_main_image: true,
-      },
-    ],
-    name: "Дерев'яний будинок",
-    price: 6000,
-  },
-  {
-    additions: [
-      'Власна ванна кміната',
-      'Туалет',
-      'Душ',
-      'Ванна',
-      'Кондиціонер',
-      'Телевізор',
-      'Холодильник',
-      'Безкоштовний wi-fi',
-      'Барбекю',
-    ],
-    id: 4,
-    images: [
-      {
-        direction: 'horizontal',
-        image: '/apartment-4.jpg',
-        is_main_image: false,
-      },
-      {
-        direction: 'vertical',
-        image: '/apartment-2.jpg',
-        is_main_image: false,
-      },
-    ],
-    name: 'VIP апартаменти',
-    price: 8000,
-  },
-]
-
 interface ApartmentsSectionProps extends HTMLAttributes<HTMLDivElement> {}
 
-const ApartmentsSection: FC<ApartmentsSectionProps> = ({
+const ApartmentsSection: FC<ApartmentsSectionProps> = async ({
   className,
   ...props
 }) => {
+  const apartments = await getApartments()
+
   return (
-    <div className={className} id="apartments" {...props}>
-      <Content>
-        <section>
-          <CurlyBraceHeading level={2} position="end">
-            Наші апартаменти
-          </CurlyBraceHeading>
-          <div className="mt-16 grid gap-[3.75rem] md:grid-cols-2 md:gap-x-28 md:gap-y-0 lg:gap-x-56">
-            {apartments.map((item) => (
-              <ApartmentsSectionItem data={item} key={item.id} />
-            ))}
-          </div>
-        </section>
-      </Content>
-    </div>
+    <>
+      {arrayIsNotEmpty(apartments) && (
+        <div className={className} id="apartments" {...props}>
+          <Content>
+            <section>
+              <CurlyBraceHeading level={2} position="end">
+                Наші апартаменти
+              </CurlyBraceHeading>
+              <div className="mt-16 grid gap-[3.75rem] md:grid-cols-2 md:gap-x-28 md:gap-y-0 lg:gap-x-56">
+                {apartments.map((item) => (
+                  <ApartmentsSectionItem data={item} key={item.id} />
+                ))}
+              </div>
+            </section>
+          </Content>
+        </div>
+      )}
+    </>
   )
 }
 
