a35e727a88ff3569874f8e2428e1ea45f0c742e2
Bochkovskyi <unundertale1@ukr.net>
2024-03-08 20:40:13 +0200

    utilize reviews fetch

--- src/components/pages/home/reviews/ReviewsSection.tsx
+++ src/components/pages/home/reviews/ReviewsSection.tsx
@@ -1,52 +1,18 @@
 import Content from '@/components/ui/Content'
 import CurlyBraceHeading from '@/components/ui/CurlyBraceHeading'
-import Review from '@/types/Review'
-import { cn } from '@/utils'
+import { cn, getReviews } from '@/utils'
 import React, { FC, HTMLAttributes } from 'react'
 
 import ReviewsSlider from './ReviewsSlider'
 
-const reviews: Review[] = [
-  {
-    author: 'Никита Овчаров',
-    date: '03.02.2022',
-    id: 1,
-    review:
-      "Рекомендуємо! Вчора їздили на чани, отримали величезне задоволення! Найкраще місце на Закарпатті, обслуговування на відмінно! Офіціант Валік кращий!	Приїдемо ще обов'язково! Дякую за чудовий вечір !",
-  },
-  {
-    author: 'dariac chai',
-    date: '14.07.2023',
-    id: 2,
-    review:
-      'Хороший вибір як для проведення великого заходу, так і для вечері вдвох, фотосесії або відпочинку на цілий день. Комплекс, розташований на горі з чудовим краєвидом. Велика доглянута територія, басейн, сауна, чани, тераси, ресторан. Хотілося б побувати скрізь, але поки що встигли тільки повечеряти в ресторані)',
-  },
-  {
-    author: 'Олег Довчанин',
-    date: '07.01.2024',
-    id: 3,
-    review:
-      'Дуже красиве місце, затишна атмосфера яку доповнює красивий каток та горячі напої, які нам порекомендували офіціанти. Також дуже смачна кухня, одним словом неймовірно нам все дуже сподобалось)',
-  },
-  {
-    author: 'Никита Овчаров',
-    date: '03.02.2022',
-    id: 4,
-    review:
-      "Рекомендуємо! Вчора їздили на чани, отримали величезне задоволення! Найкраще місце на Закарпатті, обслуговування на відмінно! Офіціант Валік кращий!	Приїдемо ще обов'язково! Дякую за чудовий вечір !",
-  },
-  {
-    author: 'Олег Довчанин',
-    date: '07.01.2024',
-    id: 5,
-    review:
-      'Дуже красиве місце, затишна атмосфера яку доповнює красивий каток та горячі напої, які нам порекомендували офіціанти. Також дуже смачна кухня, одним словом неймовірно нам все дуже сподобалось)',
-  },
-]
-
 interface ReviewsSectionProps extends HTMLAttributes<HTMLDivElement> {}
 
-const ReviewsSection: FC<ReviewsSectionProps> = ({ className, ...props }) => {
+const ReviewsSection: FC<ReviewsSectionProps> = async ({
+  className,
+  ...props
+}) => {
+  const reviews = await getReviews()
+
   return (
     <div className={cn('bg-graphite text-light', className)} {...props}>
       <Content>
