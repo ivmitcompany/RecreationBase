4c8d629a3a80640d38543f2f8189b1e95b46f25f
Bochkovskyi <unundertale1@ukr.net>
2024-03-11 11:10:19 +0200

    add meal data prop

--- src/components/pages/menu/category/MenuCategoryItem.tsx
+++ src/components/pages/menu/category/MenuCategoryItem.tsx
@@ -2,18 +2,23 @@
 
 import Modal from '@/components/ui/Modal'
 import useModal from '@/hooks/use-modal'
+import { Meal } from '@/types/KitchenCategory'
 import { cn } from '@/utils'
 import Image from 'next/image'
 import { FC, HTMLAttributes } from 'react'
 
 import MenuCategoryItemDetails from './MenuCategoryItemDetails'
 
-interface MenuCategoryItemProps extends HTMLAttributes<HTMLDivElement> {}
+interface MenuCategoryItemProps extends HTMLAttributes<HTMLDivElement> {
+  data: Meal
+}
 
 const MenuCategoryItem: FC<MenuCategoryItemProps> = ({
   className,
+  data,
   ...props
 }) => {
+  const { image, ingredients, name, price, serving_type, weight_value } = data
   const {
     closeModal: closeDetailsModal,
     modalIsOpened: detailsModalIsOpened,
@@ -32,34 +37,48 @@ const MenuCategoryItem: FC<MenuCategoryItemProps> = ({
         {...props}
       >
         <div className="font-light">
-          <h3 className="text-lg leading-5">Асорті зі свіжих овочей</h3>
-          <ul className="mt-1 flex flex-wrap gap-1 text-sm leading-4">
-            <li className="before:content-['('] after:content-[',']">огірок</li>
-            <li className="after:content-[',']">помідор</li>
-            <li className="after:content-[',']">перець болгарський</li>
-            <li className="after:content-[')']">цибуля кримська</li>
-          </ul>
+          <h3 className="text-lg leading-5">{name}</h3>
+          {ingredients && (
+            <ul className="mt-1 flex flex-wrap gap-1 text-sm leading-4">
+              {ingredients.map((ingredient, index) => (
+                <li
+                  className={cn(
+                    "after:content-[',']",
+                    index === 0 && "before:content-['(']",
+                    index === ingredients.length - 1 && "after:content-[')']"
+                  )}
+                  key={ingredient.id}
+                >
+                  {ingredient.name}
+                </li>
+              ))}
+            </ul>
+          )}
           <div className="mt-2.5 space-x-2.5">
-            <span className="text-lg text-accent">95 грн</span>
-            <span className="pb-0.5 text-sm">250г</span>
+            <span className="text-lg text-accent">{price} грн</span>
+            <span className="pb-0.5 text-sm">
+              {weight_value} {serving_type === 'mass' ? 'г' : 'шт'}
+            </span>
           </div>
         </div>
-        <div className="relative aspect-square h-full max-h-[6.5625rem] w-full max-w-[6.5625rem] flex-shrink-0 md:max-h-20 md:max-w-20">
-          <Image
-            alt="Сирники зі сметаною та джемом"
-            className="object-cover object-center"
-            fill
-            sizes="(max-width: 767px) 105px, 80px"
-            src="/meal.jpg"
-          />
-        </div>
+        {image && (
+          <div className="relative aspect-square h-full max-h-[6.5625rem] w-full max-w-[6.5625rem] flex-shrink-0 md:max-h-20 md:max-w-20">
+            <Image
+              alt="Сирники зі сметаною та джемом"
+              className="object-cover object-center"
+              fill
+              sizes="(max-width: 767px) 105px, 80px"
+              src={image}
+            />
+          </div>
+        )}
       </div>
       <Modal
         isOpen={detailsModalIsOpened}
         onClose={closeDetailsModal}
         size="card"
       >
-        <MenuCategoryItemDetails />
+        <MenuCategoryItemDetails data={data} />
       </Modal>
     </>
   )
