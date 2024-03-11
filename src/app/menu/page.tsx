af3044116d9c1bab15215b5aed9b2da1bb7a2cf2
Bochkovskyi <unundertale1@ukr.net>
2024-03-11 11:03:15 +0200

    utilize kitchen categories fetching

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,64 +1,18 @@
 import MenuCategory from '@/components/pages/menu/category/MenuCategory'
 import MeatInfo from '@/components/pages/menu/meat/MeatInfo'
+import { getKitchenCategories } from '@/utils/api/menu'
 
-const kitchenMenu = {
-  categories: [
-    {
-      additions: [
-        { id: 1, name: 'Шинка', price: 30 },
-        { id: 2, name: 'Лосось свіжий', price: 130 },
-      ],
-      id: 1,
-      meals: [
-        {
-          id: 1,
-          img: '/path-to-img',
-          name: 'Англійський сніданок',
-          price: 210,
-          weight: { type: 'mass', value: 380 },
-        },
-        {
-          id: 2,
-          name: 'Яйце варене',
-          price: 18,
-          weight: { type: 'quantity', value: 1 },
-        },
-      ],
-      name: 'Сніданки',
-      slug: 'breakfast',
-    },
-    {
-      id: 2,
-      meals: [
-        {
-          id: 1,
-          img: '/path-to-img',
-          ingredients: [
-            { id: 1, name: 'корейка свинна копчена' },
-            { id: 2, name: 'буженина' },
-            { id: 3, name: 'балик копчений' },
-          ],
-          name: 'М’ясна нарізка власного виробництва',
-          price: 285,
-          weight: { portion: 50, type: 'mass', value: 300 },
-        },
-      ],
-      name: 'Холодні закуски',
-      slug: 'cold-snacks',
-    },
-  ],
-  slug: 'kitchen',
-  title: 'Кухня',
-}
+export default async function Menu() {
+  const categories = await getKitchenCategories()
 
-export default function Menu() {
   return (
-    <>
-      <div className="space-y-[3.75rem] ">
-        <MenuCategory id="snidanki" />
-        <MenuCategory id="holodni-zakuski" />
-      </div>
-      <MeatInfo className="mt-10" />
-    </>
+    <div className="space-y-[3.75rem] ">
+      {categories.map((category) => (
+        <>
+          <MenuCategory data={category} id={category.slug} key={category.id} />
+          {category.slug === 'snidanki' && <MeatInfo className="!mt-10" />}
+        </>
+      ))}
+    </div>
   )
 }
