8a3655f8ecef343fa1fe1b7c54ff642e10561795
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 16:10:44 +0200

    refactor return statement

--- src/components/pages/menu/category/MenuCategory.tsx
+++ src/components/pages/menu/category/MenuCategory.tsx
@@ -3,7 +3,8 @@ import MenuCategoryType from '@/types/KitchenCategory'
 import { arrayIsNotEmpty, cn } from '@/utils'
 import { FC, HTMLAttributes } from 'react'
 
-import MenuCategoryItem from './MenuCategoryItem'
+import MenuCategoryItems from './MenuCategoryItems'
+import MenuSubcategory from './MenuSubcategory'
 import MenuCategoryAdditions from './additions/MenuCategoryAdditions'
 
 interface MenuCategoryProps extends HTMLAttributes<HTMLDivElement> {
@@ -12,23 +13,23 @@ interface MenuCategoryProps extends HTMLAttributes<HTMLDivElement> {
 
 const MenuCategory: FC<MenuCategoryProps> = ({
   className,
-  data: { additions, items, name, note },
+  data: { additions, items, name, note, subcategories },
   ...props
 }) => (
   <section className={cn('relative space-y-10', className)} {...props}>
     <CurlyBraceHeading level={2} position="center" size="sm">
       {name}
     </CurlyBraceHeading>
-    <p className="absolute right-0 top-[1.875rem] !mt-0 text-xs font-light md:text-sm">
-      {note}
-    </p>
-    {arrayIsNotEmpty(items) && (
-      <div className="flex flex-col gap-5">
-        {items!.map((item) => (
-          <MenuCategoryItem data={item} key={item.id} />
-        ))}
-      </div>
+    {note && (
+      <p className="absolute right-0 top-[1.875rem] !mt-0 text-xs font-light md:text-sm">
+        {note}
+      </p>
     )}
+    {arrayIsNotEmpty(subcategories) &&
+      subcategories!.map((subcategory) => (
+        <MenuSubcategory data={subcategory} key={subcategory.id} />
+      ))}
+    {arrayIsNotEmpty(items) && <MenuCategoryItems data={items!} />}
     {arrayIsNotEmpty(additions) && <MenuCategoryAdditions data={additions!} />}
   </section>
 )
