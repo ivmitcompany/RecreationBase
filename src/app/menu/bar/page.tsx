a17693f301d839ce174abae148a607da6200f708
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 16:11:18 +0200

    utilize bar categories fetching

--- src/app/menu/bar/page.tsx
+++ src/app/menu/bar/page.tsx
@@ -1,3 +1,14 @@
-export default function Bar() {
-  return <div className="space-y-[3.75rem] "></div>
+import MenuCategory from '@/components/pages/menu/category/MenuCategory'
+import { getBarCategories } from '@/utils/api/menu'
+
+export default async function Bar() {
+  const categories = await getBarCategories()
+
+  return (
+    <div className="space-y-[3.75rem] ">
+      {categories.map((category) => (
+        <MenuCategory data={category} id={category.slug} key={category.id} />
+      ))}
+    </div>
+  )
 }
