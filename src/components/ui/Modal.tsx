5e4e038d0ac83597b3fa8df8f8616e639db1450a
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 14:36:20 +0200

    update card variant styles

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -27,7 +27,7 @@ const modalBodyVariants = cva('relative bg-light text-dark', {
   defaultVariants: { size: 'default' },
   variants: {
     size: {
-      card: 'w-full h-[71dvh] md:h-full md:max-h-[36.875rem] md:max-w-[36.875rem] overflow-y-auto',
+      card: 'w-full max-h-[35.9375rem] h-full md:max-h-[36.875rem] md:max-w-[36.875rem] overflow-y-auto',
       default:
         'w-full h-full md:max-h-[43.75rem] md:max-w-[68.75rem] px-5 py-20 md:py-[3.75rem] md:px-[3.25rem] overflow-y-auto md:overflow-hidden',
       screen:
