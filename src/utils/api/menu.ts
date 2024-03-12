f6410d60fa9d4b953c1df085d4e2d68ab88f3c4b
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 17:22:03 +0200

    add getNavigationCategories utility

--- src/utils/api/menu.ts
+++ src/utils/api/menu.ts
@@ -1,4 +1,5 @@
 import MenuCategoryType from '@/types/KitchenCategory'
+import MenuNavPathname from '@/types/MenuNavPathname'
 import requestSender from '@/types/classes/RequestSender'
 
 export const getKitchenCategories = async () => {
@@ -11,4 +12,12 @@ export const getKitchenCategories = async () => {
 export const getBarCategories = async () => {
   const data = await requestSender.get<MenuCategoryType[]>('/bar/categories')
   return data
+}
+
+export const getNavigationCategories = async (pathname: MenuNavPathname) => {
+  if (pathname === '/menu') {
+    return await getKitchenCategories()
+  } else {
+    return await getBarCategories()
+  }
 }
