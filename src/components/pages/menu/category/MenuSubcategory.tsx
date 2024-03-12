9264cff503ceaee3499b59041e7c513d98015640
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 16:09:56 +0200

    add MenuSubcategory component

---
+++ src/components/pages/menu/category/MenuSubcategory.tsx
@@ -0,0 +1,22 @@
+import { MenuSubcategoryType } from '@/types/KitchenCategory'
+import { arrayIsNotEmpty, cn } from '@/utils'
+import { FC, HTMLAttributes } from 'react'
+
+import MenuCategoryItems from './MenuCategoryItems'
+
+interface MenuSubcategoryProps extends HTMLAttributes<HTMLDivElement> {
+  data: MenuSubcategoryType
+}
+
+const MenuSubcategory: FC<MenuSubcategoryProps> = ({
+  className,
+  data: { items, name },
+  ...props
+}) => (
+  <div className={cn('space-y-[1.875rem] md:space-y-5', className)} {...props}>
+    <h3 className="text-end text-lg font-light uppercase">{name}</h3>
+    {arrayIsNotEmpty(items) && <MenuCategoryItems data={items!} />}
+  </div>
+)
+
+export default MenuSubcategory
