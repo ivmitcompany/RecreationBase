d117468c3e3561d9078ad705cd8d5b6580ff5bed
Bochkovskyi <unundertale1@ukr.net>
2024-03-12 15:18:38 +0200

    add bar categories fetch utility

--- src/utils/api/menu.ts
+++ src/utils/api/menu.ts
@@ -1,7 +1,14 @@
-import KitchenCategory from '@/types/KitchenCategory'
+import MenuCategoryType from '@/types/KitchenCategory'
 import requestSender from '@/types/classes/RequestSender'
 
 export const getKitchenCategories = async () => {
-  const data = await requestSender.get<KitchenCategory[]>('/kitchen/categories')
+  const data = await requestSender.get<MenuCategoryType[]>(
+    '/kitchen/categories'
+  )
+  return data
+}
+
+export const getBarCategories = async () => {
+  const data = await requestSender.get<MenuCategoryType[]>('/bar/categories')
   return data
 }
