0798e1ad9698667ca7123e0ce5213b3669bfdc8f
Bochkovskyi <unundertale1@ukr.net>
2024-03-23 08:45:45 +0200

    update site url constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const DESKTOP_BREAKPOINT = 1279.99
 export const MOBILE_BREAKPOINT = 767.99
 
-export const SITE_URL = 'http://localhost:3000'
+export const SITE_URL = 'https://recreation-base.vercel.app'
