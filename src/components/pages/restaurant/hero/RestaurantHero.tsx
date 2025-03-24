26b405eec7a9095084d58c335c80939fa2c7e49a
KostakViktoriia <KostakV@finsfera.com.ua>
2025-03-24 16:20:18 +0200

    Оновлено стилі HeroSection та RestaurantHero, додано фон зображення та атрибути preload і poster для відео.

--- src/components/pages/restaurant/hero/RestaurantHero.tsx
+++ src/components/pages/restaurant/hero/RestaurantHero.tsx
@@ -22,6 +22,8 @@ const RestaurantHero: FC<RestaurantHeroProps> = ({ className, ...props }) => {
           loop
           muted
           playsInline
+          preload="auto"
+          poster="/restaurant-hero.jpeg"
         >
           <source
             src="https://res.cloudinary.com/db8gndp2b/video/upload/v1742418835/IMG_1996_adebbz.mp4"
